b852ec19802eb895a11d855dfdeb3519848c9e64
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2016-03-16 11:38:14 +0100

    Changed Socket to UdpClient

--- I4IKN-GR4/Exercise-9/file_client/file_client.cs
+++ I4IKN-GR4/Exercise-9/file_client/file_client.cs
@@ -9,6 +9,7 @@ namespace udp
 	public class UDPClient
 	{
 		private const int serverPort = 9000;
+		private static UdpClient client;
 
 		static void Main(string[] args) {
 
@@ -16,18 +17,20 @@ namespace udp
 			if (args.Length > 0) {
 				argument = args [0];
 			} else {
-				Console.WriteLine (">> Type in a string to send to server...");
+				Console.WriteLine (">> Type L or U to receive statistics from the server...");
 				argument = Console.ReadLine ();
 			}
 
-			// More UDP
-			Socket socket = new Socket (AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-			IPAddress broadcast = IPAddress.Parse ("10.0.0.1");
+			// Setup client
+			IPAddress address = IPAddress.Parse ("10.0.0.1");
+			IPEndPoint endPoint = new IPEndPoint (address, serverPort);
+			client = new UdpClient(endPoint);
 
-			// UDP
+			// Send
 			byte[] sendBuf = Encoding.ASCII.GetBytes(argument);
-			IPEndPoint endPoint = new IPEndPoint (broadcast, serverPort);
-			socket.SendTo (sendBuf, endPoint);
+			client.Send(sendBuf, sendBuf.Length ,endPoint);
+
+			// Done
 			Console.WriteLine (">> " + argument + " send to server");
 			Console.ReadKey ();
 		}
