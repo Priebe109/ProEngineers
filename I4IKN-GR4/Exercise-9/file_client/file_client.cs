58f00be3f79500a85adfc5053fbaebeedf162b63
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2016-03-18 10:54:40 +0100

    Refactored client

--- I4IKN-GR4/Exercise-9/file_client/file_client.cs
+++ I4IKN-GR4/Exercise-9/file_client/file_client.cs
@@ -11,16 +11,8 @@ namespace udp
 		private const int serverPort = 9000;
 		private static UdpClient client;
 
-		static void Main(string[] args) {
-
-			string argument;
-			if (args.Length > 0) {
-				argument = args [0];
-			} else {
-				Console.WriteLine (">> Type L or U to receive statistics from the server...");
-				argument = Console.ReadLine ();
-			}
-
+		static void StartClient(string argument)
+		{
 			// Setup client
 			IPAddress address = IPAddress.Parse ("10.0.0.1");
 			IPEndPoint endPoint = new IPEndPoint (address, serverPort);
@@ -36,6 +28,20 @@ namespace udp
 			byte[] receiveBuf = client.Receive (ref groupEndPoint);
 			Console.WriteLine (">> Response from server:");
 			Console.WriteLine(Encoding.ASCII.GetString(receiveBuf, 0, receiveBuf.Length));
+		}
+
+		static void Main(string[] args)
+		{
+			string argument;
+			if (args.Length > 0) {
+				argument = args [0];
+			} else {
+				Console.WriteLine (">> Type L or U to receive statistics from the server...");
+				argument = Console.ReadLine ();
+			}
+
+			// Start
+			StartClient(argument);
 
 			// Done
 			Console.ReadKey ();
