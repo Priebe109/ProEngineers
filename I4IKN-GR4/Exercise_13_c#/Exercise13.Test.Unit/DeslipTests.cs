853d889a8b12a5d2accd179b5fa426da7849afad
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2016-04-26 11:01:40 +0200

    Deslip adjusted and deslip unit test added

---
+++ I4IKN-GR4/Exercise_13_c#/Exercise13.Test.Unit/DeslipTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Exercise13.Test.Unit
+{
+	[TestFixture]
+	public class DeslipTests
+	{
+		const byte DELIMITER = (byte)'A';
+
+		[SetUp]
+		public DeslipTests ()
+		{
+		}
+	}
+}
