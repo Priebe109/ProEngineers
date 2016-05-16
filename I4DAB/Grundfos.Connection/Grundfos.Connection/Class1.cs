e76ab52341a0bef192562c994a0c6b0b2c4076c8
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2016-05-16 14:54:05 +0200

    Connection project added

---
+++ I4DAB/Grundfos.Connection/Grundfos.Connection/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Net;
+
+namespace Grundfos.Connection
+{
+    public class DataRetriever
+    {
+
+
+
+
+        public void GetJSONResponse(string url)
+        {
+            // Create request
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = WebRequestMethods.Http.Get;
+            request.Accept = "application/json";
+
+
+            string text;
+            var response = (HttpWebResponse)request.GetResponse();
+
+            using (var sr = new StreamReader(response.GetResponseStream()))
+            {
+                text = sr.ReadToEnd();
+            }
+        }
+    }
+}
