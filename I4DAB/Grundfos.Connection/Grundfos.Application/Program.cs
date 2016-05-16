d80dd29768f1fad6143d32bc78b0986bd1edd2ba
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2016-05-16 15:09:19 +0200

    DataRetriever tested with application

--- I4DAB/Grundfos.Connection/Grundfos.Application/Program.cs
+++ I4DAB/Grundfos.Connection/Grundfos.Application/Program.cs
@@ -1,4 +1,6 @@
-﻿using Grundfos.Connection;
+﻿using System;
+using System.Runtime.InteropServices;
+using Grundfos.Connection;
 
 namespace Grundfos.Application
 {
@@ -8,6 +10,10 @@ namespace Grundfos.Application
         {
             var dataRetriever = new DataRetriever();
 
+            for (int i = 1; i < 5; i++)
+                Console.WriteLine(dataRetriever.GetJsonResponse(string.Format($"http://userportal.iha.dk/~jrt/i4dab/E14/HandIn4/dataGDL/data/{i}.json")));
+
+            Console.ReadKey();
         }
     }
 }
