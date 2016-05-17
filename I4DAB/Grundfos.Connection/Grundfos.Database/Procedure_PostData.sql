e800e4d08392d691d873c72f89336034993beb35
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2016-05-17 17:55:08 +0200

    PostData procedure done

--- I4DAB/Grundfos.Connection/Grundfos.Database/Procedure_PostData.sql
+++ I4DAB/Grundfos.Connection/Grundfos.Database/Procedure_PostData.sql
@@ -2,13 +2,16 @@
 GO
 
 CREATE PROCEDURE [dbo].[Procedure_PostData]
+(
+	@value int,
+	@timestamp datetime,
+	@sensorId int,
 	@apartmentId int
+)
 AS
 BEGIN
-	SELECT
-		*
-	FROM
-		[dbo].[ReadingsForApartment](@apartmentId)
-	ORDER BY
-		readingTimestamp ASC
+	INSERT INTO
+		reading (readingValue, readingTimestamp, apartmentId, sensorId)
+	VALUES
+		(@value, @timestamp, @apartmentId, @sensorId)
 END
--- I4DAB/Grundfos.Connection/Grundfos.Database/Procedure_Test.sql
+++ I4DAB/Grundfos.Connection/Grundfos.Database/Procedure_Test.sql
@@ -1,4 +1,7 @@
-﻿/* Gets readings from specified flat id */
+﻿/* Gets readings from specified apartment id */
 EXECUTE Procedure_GetData 69
+GO
 
-/*  */
+/* Posts reading */
+EXECUTE Procedure_PostData @value = 10, @timestamp = GETDATE, @sensorId = 10, @apartmentId = 10
+GO
