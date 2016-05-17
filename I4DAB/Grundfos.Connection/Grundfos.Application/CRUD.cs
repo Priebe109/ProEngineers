24f7a2881480f3a2794083daab4e26b595e3606c
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2016-05-17 12:08:24 +0200

    CRUD methods added

--- I4DAB/Grundfos.Connection/Grundfos.Application/CRUD.cs
+++ I4DAB/Grundfos.Connection/Grundfos.Application/CRUD.cs
@@ -26,16 +26,16 @@ namespace Grundfos.Application
             }
         }
 
-        public void CreateSensor(string description, string unit, DateTime calibrationDate)
+        public void CreateSensor(string description, string unit, DateTime calibrationDate, string calEq, string calCoef, string exRef)
         {
             var sensor = new sensorCharacteristic()
             {
                 description = description,
                 unit = unit,
                 calibrationDate = calibrationDate,
-                calibrationEquation = "",
-                calibrationCoefficient = "",
-                externalRef = ""
+                calibrationEquation = calEq,
+                calibrationCoefficient = calCoef,
+                externalRef = exRef
             };
 
             using (var model = new GrundfosModel())
@@ -64,8 +64,74 @@ namespace Grundfos.Application
 
         // Read
 
+        public List<reading> ReadingsForApartment(long id)
+        {
+            using (var model = new GrundfosModel())
+            {
+                var readings = model.readings.Where(_reading => _reading.apartmentId == id);
+                return readings.ToList();
+            }
+        }
+
+        public List<apartmentCharacteristic> Apartments()
+        {
+            using (var model = new GrundfosModel())
+            {
+                var apartments = model.apartmentCharacteristics;
+                return apartments.ToList();
+            }
+        }
+
         // Update
 
+        public void UpdateSensorCalibration(long id, DateTime calibrationDate, string calEq, string calCoef)
+        {
+            using (var model = new GrundfosModel())
+            {
+                var sensorQuery = model.sensorCharacteristics.Where(_sensor => _sensor.sensorId == id);
+                var sensor = sensorQuery.FirstOrDefault();
+
+                sensor.calibrationDate = calibrationDate;
+                sensor.calibrationEquation = calEq;
+                sensor.calibrationCoefficient = calCoef;
+
+                model.SaveChanges();
+            }
+        }
+
         // Delete
+
+        public void DeleteSensor(long id)
+        {
+            using (var model = new GrundfosModel())
+            {
+                var sensorQuery = model.sensorCharacteristics.Where(_sensor => _sensor.sensorId == id);
+                var sensor = sensorQuery.FirstOrDefault();
+                model.sensorCharacteristics.Remove(sensor);
+                model.SaveChanges();
+            }
+        }
+
+        public void DeleteApartment(long id)
+        {
+            using (var model = new GrundfosModel())
+            {
+                var apartmentQuery = model.apartmentCharacteristics.Where(_apartment => _apartment.apartmentId == id);
+                var apartment = apartmentQuery.FirstOrDefault();
+                model.apartmentCharacteristics.Remove(apartment);
+                model.SaveChanges();
+            }
+        }
+
+        public void DeleteReading(long id)
+        {
+            using (var model = new GrundfosModel())
+            {
+                var readingQuery = model.readings.Where(_reading => _reading.readingId == id);
+                var reading = readingQuery.FirstOrDefault();
+                model.readings.Remove(reading);
+                model.SaveChanges();
+            }
+        }
     }
 }
