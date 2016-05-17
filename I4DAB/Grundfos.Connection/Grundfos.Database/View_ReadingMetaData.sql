959d2dfa3740b0934848987f9dbe7c6b8f11d559
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2016-05-17 18:54:15 +0200

    View added and design reverse engineered

---
+++ I4DAB/Grundfos.Connection/Grundfos.Database/View_ReadingMetaData.sql
@@ -0,0 +1,29 @@
+﻿USE [Grundfos.Database]
+GO
+
+CREATE VIEW View_ReadingMetaData
+AS
+SELECT
+	TABLE_CATALOG,
+	TABLE_SCHEMA,
+	TABLE_NAME,
+	COLUMN_NAME,
+	DATA_TYPE
+FROM
+	[Grundfos.Database].INFORMATION_SCHEMA.COLUMNS
+WHERE
+	TABLE_NAME = 'reading'
+
+/* Previous attempt (using reading function data)
+
+CREATE VIEW View_ReadingMetaData
+AS
+SELECT
+	COUNT(readingValue),
+	AVG(readingValue),
+	MAX(readingValue),
+	MIN(readingValue),
+	MAX(readingTimestamp)
+FROM
+	reading
+*/
