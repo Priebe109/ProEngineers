75587f7e0ee7ed5c2488af88a5d097eb5335a5de
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2016-06-18 17:32:42 +0200

    TDD UltraCreditCardValidator first two requirements tested and implemented

---
+++ I4SWT-GR1/UltraCreditCardValidator/UltraCreditCardValidator.Test.Unit/CreditCardValidatorTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace UltraCreditCardValidator.Test.Unit
+{
+    [TestFixture]
+    public class CreditCardValidatorTests
+    {
+        private CreditCardValidator _uut;
+
+        [SetUp]
+        public void CreateValidator()
+        {
+            _uut = new CreditCardValidator();
+        }
+
+        [TestCase("John Thomas Snow", true)]
+        [TestCase("John T. Snow", true)]
+        [TestCase("John Snow", true)]
+        [TestCase("J Snow", false)]
+        [TestCase("J. Snow", false)]
+        [TestCase("John S.", false)]
+        public void ValidateName_ParameterizedName_ReturnsTrueIfNameIsValid(string name, bool shouldSucceed)
+        {
+            var succeeded = _uut.ValidateName(name);
+            Assert.That(succeeded, Is.EqualTo(shouldSucceed));
+        }
+
+        [TestCase("Visa", true)]
+        [TestCase("MasterCard", true)]
+        [TestCase("American Express", true)]
+        [TestCase("Dankort", false)]
+        public void ValidateCreditCardType_ParameterizedType_ReturnsTrueIfTypeIsValid(string type, bool shouldSucceed)
+        {
+            var succeeded = _uut.ValidateCreditCardType(type);
+            Assert.That(succeeded, Is.EqualTo(shouldSucceed));
+        }
+    }
+}
---
+++ I4SWT-GR1/UltraCreditCardValidator/UltraCreditCardValidator/CreditCardValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltraCreditCardValidator
+{
+    public class CreditCardValidator
+    {
+        private const int MinimumNumberOfNamesRequired = 2;
+        private readonly string[] _acceptedCreditCardTypes = {"Visa", "MasterCard", "American Express"};
+
+        public bool ValidateName(string name)
+        {
+            var names = name.Split(' ');
+
+            return
+                names.Length >= MinimumNumberOfNamesRequired
+                && NameIsNotInitial(names.First())
+                && NameIsNotInitial(names.Last());
+        }
+
+        private bool NameIsNotInitial(string name)
+        {
+            return name.Length > 1 && name.Last() != '.';
+        }
+
+        public bool ValidateCreditCardType(string type)
+        {
+            return _acceptedCreditCardTypes.Contains(type);
+        }
+    }
+}
